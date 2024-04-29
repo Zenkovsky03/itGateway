a483de8af7fd24f4fc6cb8260655a06307e7d230
Matthew <mateuszzaniewski1@gmail.com>
2024-04-29 23:57:33 +0200

    create app: job

---
+++ job/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Job(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+    email = models.EmailField(null=True)
+
+
+    def __str__(self):
+        return self.title
