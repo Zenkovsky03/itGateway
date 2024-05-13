5fb574ffcdb10644605ac84dd3d1fdbabe3c7f60
Matthew <mateuszzaniewski1@gmail.com>
2024-05-13 10:15:16 +0200

    add: basic serializer to test, then i separate it from core but now it woult be test

--- job/views.py
+++ job/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from requests import Response
+from rest_framework.decorators import api_view
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_all_jobs(request):
+
+    jobs = Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
